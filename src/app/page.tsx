01cbbc53ee37c895c5485d9eb77ae1fcb5261b14
famgz <famgz@proton.me>
2024-08-20 16:39:02 -0300

    feat: add form configuration and initial home page form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import PatientForm from '@/components/forms/patient-form';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="asd flex h-screen max-h-screen">
+    <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -11,12 +12,29 @@ export default function Home() {
             height={100}
             width={100}
             alt="Logo"
-            className="mb-12 h-10 w-auto"
+            className="mb-12 h-10 w-fit"
           />
-        </div>
 
-        <PatientForm />
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
       </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="Patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
--- src/components/forms/patient-form.tsx
+++ src/components/forms/patient-form.tsx
@@ -1,5 +1,93 @@
+'use client';
+
+import CustomFormField from '@/components/custom-form-field';
+import SubmitButton from '@/components/submit-button';
+import { Form } from '@/components/ui/form';
+import { UserFormValidation } from '@/lib/validation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+export enum FormFieldType {
+  INPUT = 'input',
+  TEXTAREA = 'textarea',
+  PHONE_INPUT = 'phoneInput',
+  CHECKBOX = 'checkbox',
+  DATE_PICKER = 'datePicker',
+  SELECT = 'select',
+  SKELETON = 'skeleton',
+}
+
 const PatientForm = () => {
-  return <div></div>;
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: '',
+      email: '',
+      phone: '',
+    },
+  });
+
+  async function onSubmit({
+    name,
+    email,
+    phone,
+  }: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+    try {
+      // const userData = { name, email, phone, };
+      // const user = await createUser(userData);
+      // if (user) router.push(`/patients/${user.id}/register`);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-6">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there!</h1>
+          <p className="text-dark-700">Schedule your first appointment</p>
+        </section>
+
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="name"
+          label="Full name"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="john@email.me"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="Phone"
+          placeholder="(011) 96969-6969"
+        />
+
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+      </form>
+    </Form>
+  );
 };
 
 export default PatientForm;
